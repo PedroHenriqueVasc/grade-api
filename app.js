01969558a5d36553e8fb1b2c1e048c445b294c9c
Pedro Henrique de Vasconcelos <45134146+PedroHenriqueVasc@users.noreply.github.com>
2020-07-10 10:08:14 -0300

    atualizacao do cors

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://still-shelf-93793.herokuapp.com/',
+    origin: 'https://still-shelf-93793.herokuapp.com',
   })
 );
 
